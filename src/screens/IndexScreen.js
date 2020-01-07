965975d2f08f6a6fbb3d6f8e8154bec63fbb720a
Nick Hardwick <nickghardwick@gmail.com>
2020-01-06 21:04:33 -0800

    Blog posts context added

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,20 @@
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  let value = useContext(BlogContext);
+  let blogPosts = useContext(BlogContext);
 
   return (
     <View>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({item}) => {
+            return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
