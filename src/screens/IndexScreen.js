22d3403fcc8a721fa19d837eb5531f1505a71fb3
Nick Hardwick <nickghardwick@gmail.com>
2020-01-09 19:00:10 -0800

    Updated reducer to handle deletion of blog posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,10 @@
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet, FlatList, Button} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-native';
 import {Context} from '../context/BlogContext';
 import {Feather} from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const {state, addBlogPost} = useContext(Context);
+  const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
   return (
     <View>
@@ -16,10 +16,14 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({item}) => {
-            return <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
+          return (
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title} - {item.id}</Text>
+            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
               <Feather style={styles.icon} name="trash" />
-            </View>
+            </TouchableOpacity>
+          </View>
+          );
         }}
       />
     </View>
