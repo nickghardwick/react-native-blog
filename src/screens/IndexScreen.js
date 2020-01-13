42b68288dc215d22311b30384b9cd19215c02a34
Nick Hardwick <nickghardwick@gmail.com>
2020-01-13 11:37:47 -0500

    "Added ShowScreen to see blog posts"

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-
 import {Context} from '../context/BlogContext';
 import {Feather} from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
   const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
   return (
@@ -17,12 +17,14 @@ const IndexScreen = () => {
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({item}) => {
           return (
-          <View style={styles.row}>
-            <Text style={styles.title}>{item.title} - {item.id}</Text>
-            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-              <Feather style={styles.icon} name="trash" />
+            <TouchableOpacity onPress={() => navigation.navigate('Show', {id: item.id})}>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
             </TouchableOpacity>
-          </View>
           );
         }}
       />
