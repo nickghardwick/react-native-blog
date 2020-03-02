9f0577304abe4c522ef7e93b91bfdc550578090d
Nick Hardwick <nickghardwick@gmail.com>
2020-03-02 15:55:46 -0500

    Edit icon link added

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,8 @@
 import React, {useContext} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Context} from '../context/BlogContext';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {EvilIcons} from '@expo/vector-icons';
 
 const ShowScreen = ({navigation}) => {
     const {state} = useContext(Context);
@@ -10,8 +12,19 @@ const ShowScreen = ({navigation}) => {
     return (
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     );
+};
+
+ShowScreen.navigationOptions = () => {
+    return {
+        headerRight: (
+        <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+            <EvilIcons name="pencil" size={35} />
+        </TouchableOpacity>
+        )
+    };
 };
 
 const styles = StyleSheet.create({});
